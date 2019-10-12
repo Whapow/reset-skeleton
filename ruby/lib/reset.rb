9cbd4a26c6adce44f74ff0dae4373933d8334313
Joshua Van Camp <joshuavancamp+github@gmail.com>
2019-10-12 12:18:45 -0400

    Coding Challenge - Ruby (#1)
    * in progress 1 - coffee break
    * finish tests

--- ruby/lib/reset.rb
+++ ruby/lib/reset.rb
@@ -2,4 +2,68 @@
 
 class Reset
   VERSION = '1.0.0'
+
+  def initialize(array = [])
+    @set = array.sort.uniq
+  end
+
+  def set
+    @set
+  end
+
+  def print
+    "{ #{@set.join(', ')} }"
+  end
+
+  def contains?(value)
+    @set.include?(value)
+  end
+
+  def empty?
+    @set.empty?
+  end
+
+  def union(reset)
+    raise ArgumentError.new("Must be a Reset") unless reset.is_a?(Reset)
+    u = @set.to_a
+    reset.set.each do |i|
+      u.push(i) unless self.contains?(i)
+    end
+    Reset.new(u)
+  end
+  
+  def intersection(reset)
+    raise ArgumentError.new("Must be a Reset") unless reset.is_a?(Reset)
+    n = []
+    reset.set.each do |i|
+      n.push(i) if self.contains?(i)
+    end
+    Reset.new(n)
+  end
+  
+  def difference(reset)
+    raise ArgumentError.new("Must be a Reset") unless reset.is_a?(Reset)
+    d = []
+    @set.each do |i|
+      d.push(i) unless reset.contains?(i)
+    end
+    Reset.new(d)
+  end
+  
+  def has_subset?(reset)
+    raise ArgumentError.new("Must be a Reset") unless reset.is_a?(Reset)
+    reset.set.each do |i|
+      return false unless self.contains?(i)
+    end
+    true
+  end
+
+  def subset_of?(reset)
+    raise ArgumentError.new("Must be a Reset") unless reset.is_a?(Reset)
+    @set.each do |i|
+      return false unless reset.contains?(i)
+    end
+    true
+  end
+
 end
--- ruby/test/test_reset.rb
+++ ruby/test/test_reset.rb
@@ -5,4 +5,105 @@ require "minitest/autorun"
 require "reset"
 
 class TestReset < Minitest::Test
+  
+  describe "Reset" do
+    
+    before do
+      @test_numbers = Reset.new([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+      @single_digits = Reset.new([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+      @odds = Reset.new([1, 3, 5, 7, 9, 11, 13, 15, 17, 19])
+      @evens = Reset.new([2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
+      @primes = Reset.new([2,3,5,7,11,13,17,19])
+    end
+    
+    describe "print" do
+      it 'displays the (re)set in the proper format' do
+        assert_equal "{ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 }", @odds.print
+      end
+    end
+    
+    describe "contains?" do
+      it "contains the given value" do
+        assert @odds.contains?(3) && @evens.contains?(4)
+      end
+      
+      it "does not contain the given value" do
+        refute @evens.contains?(0) || @primes.contains?(1)
+      end
+    end
+    
+    describe "empty?" do
+      it "is empty" do
+        empty = Reset.new()
+        assert empty.empty?
+      end
+
+      it "is not empty" do
+        refute @odds.empty?
+      end
+    end
+
+    describe "multi-set methods" do
+
+      it "give an ArgumentError when given a non-set" do
+        errors = []
+
+        errors.push(assert_raises {@test_numbers.has_subset?([1, 2, 3])})
+        errors.push(assert_raises {@test_numbers.has_subset?("{ 1, 2, 3 }")})
+
+        errors.push(assert_raises {@test_numbers.subset_of?([1, 2, 3])})
+        errors.push(assert_raises {@test_numbers.subset_of?("{ 1, 2, 3 }")})
+
+        errors.push(assert_raises {@test_numbers.union([1, 2, 3])})
+        errors.push(assert_raises {@test_numbers.union("{ 1, 2, 3 }")})
+
+        errors.push(assert_raises {@test_numbers.intersection([1, 2, 3])})
+        errors.push(assert_raises {@test_numbers.intersection("{ 1, 2, 3 }")})
+
+        errors.push(assert_raises {@test_numbers.difference([1, 2, 3])})
+        errors.push(assert_raises {@test_numbers.difference("{ 1, 2, 3 }")})
+        
+        errors.each do |error|
+          assert_match "Must be a Reset", error.message
+          assert_equal ArgumentError, error.class
+        end
+      end
+
+      describe "has_subset?" do
+        it 'checks if a given (re)set is a subset of the (re)set' do
+          assert @test_numbers.has_subset?(@evens)
+        end
+      end
+      
+      describe "subset_of?" do
+        it 'checks if the (re)set is a subset of a give (re)set' do
+          assert @odds.subset_of?(@test_numbers)
+        end
+      end
+
+      describe "union" do
+        it 'create a union of the two (re)sets' do
+          expected = Reset.new([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 17, 19])
+          single_digits_and_primes = @single_digits.union(@primes)
+          assert_equal expected.print, single_digits_and_primes.print
+        end
+      end
+      
+      describe "intersection" do  
+        it 'creates an intersection of the two (re)sets' do
+          expected = Reset.new([2, 3, 5, 7])
+          single_digit_primes = @single_digits.intersection(@primes)
+          assert_equal expected.print, single_digit_primes.print
+        end
+      end
+      
+      describe "difference" do  
+        it 'gives the difference of the two (re)sets' do
+          expected = Reset.new([0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20 ])
+          nonprimes = @test_numbers.difference(@primes)
+          assert_equal expected.print, nonprimes.print
+        end
+      end
+    end
+  end
 end
